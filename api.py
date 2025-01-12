4f917273cc5f3abb5e4c2559704d3e506bbd9c58
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-01-12 15:16:22 -0500

    content added to api endpoint

--- api.py
+++ api.py
@@ -2,35 +2,107 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/data_structures/")
-
-def DS():
+@app.get("/data_structures")
+def DS_info():
     return [
         {
-            "name": "Array"
+            "name": "Array",
+            "Linear": "yes",
+            "Data_structure":"array",
+            "Operation_complexity":{
+                "insert":"constant",
+                "delete":"constant",
+                "access":"constant",
+                "search":"linear",
+            },
         },
         {
-            "name": "Vector"
+            "name": "Vector",
+            "Linear": "yes",
+            "Data_structure":"dynamic array",
+            "Operation_complexity":{
+                "insert":"constant",
+                "delete":"constant",
+                "access":"constant",
+                "search":"linear",
+            },
         },
         {
-            "name": "Linked List"
+            "name": "List",
+            "Linear": "yes",
+            "Data_structure":"doubly linked list",
+            "Operation_complexity":{
+                "insert":"constant",
+                "delete":"constant",
+                "access":"linear",
+                "search":"linear",
+            },
         },
         {
-            "name": "Stack"
+            "name": "Stack",
+            "Linear": "yes",
+            "Data_structure":"stack",
+            "Operation_complexity":{
+                "insert":"constant",
+                "delete":"constant",
+                "access":"constant",
+                "search":"N/A",
+            },
         },
         {
-            "name": "Queue"
+            "name": "Queue",
+            "Linear": "yes",
+            "Data_structure":"queue",
+            "Operation_complexity":{
+                "insert":"constant",
+                "delete":"constant",
+                "access":"constant",
+                "search":"N/A",
+            },
         },
         {
-            "name": "Ordered_set"
+            "name": "set",
+            "Linear": "no",
+            "Data_structure":"binary search tree",
+            "Operation_complexity":{
+                "insert":"logarithmic",
+                "delete":"logarithmic",
+                "access":"logarithmic",
+                "search":"logarithmic",
+            },
+
         },
         {
-            "name": "Ordered_map"
+            "name": "map",
+            "Linear": "no",
+            "Data_structure":"binary search tree",
+            "Operation_complexity":{
+                "insert":"logarithmic",
+                "delete":"logarithmic",
+                "access":"logarithmic",
+                "search":"logarithmic",
+            },
         },
         {
-            "name": "Unordered_set"
+            "name": "Unordered_set",
+            "Linear": "no",
+            "Data_structure":"hash table",
+            "Operation_complexity":{
+                "insert":"constant",
+                "delete":"constant",
+                "access":"constant",
+                "search":"constant",
+            },
         },
         {
-            "name": "Unordered_map"
+            "name": "Unordered_map",
+            "Linear": "no",
+            "Data_structure":"hash table",
+            "Operation_complexity":{
+                "insert":"constant",
+                "delete":"constant",
+                "access":"constant",
+                "search":"constant",
+            },
         },
     ]
