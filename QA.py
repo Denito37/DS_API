b29866821e3ba8b0eeeb982e7b9e02e768116bd4
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-01-16 23:56:28 -0500

    added new data table & model

--- QA.py
+++ QA.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException
 import sqlalchemy.orm
 import uvicorn
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 import sqlalchemy
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker, Session, relationship
 from pydantic import BaseModel
 from typing import Dict, List
 from fastapi.middleware.cors import CORSMiddleware
+from json import dump
 
 app = FastAPI()
 
@@ -18,9 +19,18 @@ Base = sqlalchemy.orm.declarative_base()
 class Question(Base):
     __tablename__="Questions"
     id = Column(Integer, primary_key=True, index=True)
-    Question = Column(String, index=True)
-    #answers = Column(Dict[str,str])
+    Question = Column(String)
     Correct_Answer = Column(String)
+    answers = relationship('Answers', backref='Questions')
+
+class Answers(Base):
+    __tablename__='Answers'
+    id = Column(Integer, primary_key=True, index=True)
+    choice_A = Column(String)
+    choice_B = Column(String)
+    choice_C = Column(String)
+    choice_D = Column(String)
+    question_id = Column(Integer, ForeignKey('Questions.id'))
 
 Base.metadata.create_all(bind=engine)
 
@@ -33,16 +43,27 @@ def get_db():
 
 class Question_create(BaseModel):
     Question:str
-    #Answers:Dict[str,str]
     Correct_Answer:str
 
-class Question_structure(BaseModel):
+class Question_response(BaseModel):
     id:int
     Question:str
-    #Answers:Dict[str,str]
     Correct_Answer:str
 
-@app.post("/Questions",response_model= Question_structure)
+class Answer_create(BaseModel):
+    choice_A:str
+    choice_B:str
+    choice_C:str
+    choice_D:str
+
+class Answer_response(BaseModel):
+    id:int
+    choice_A:str
+    choice_B:str
+    choice_C:str
+    choice_D:str
+
+@app.post("/Questions",response_model= Question_response)
 async def add_Question(question: Question_create, db : Session =Depends(get_db)):
     db_item = Question(**question.model_dump())
     db.add(db_item)
@@ -50,9 +71,17 @@ async def add_Question(question: Question_create, db : Session =Depends(get_db))
     db.refresh(db_item)
     return db_item
 
-@app.get("/Questions",response_model= List[Question_structure])
+@app.post("/Answers", response_model=Answer_response)
+async def add_Answers(answer: Answer_create, db : Session = Depends(get_db)):
+    db_item = Answers(**answer.model_dump())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+@app.get("/Questions")
 async def get_Question(db : Session =Depends(get_db)):
-    db_item = db.query(Question).all()
+    db_item = db.query(Question, Answers).all()
     return db_item
 
 
